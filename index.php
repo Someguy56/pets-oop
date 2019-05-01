16e947ed5a9c8841b425a553afb8f9b8de7b77ba
Max Lee <maxwell.a.lee56@gmail.com>
2019-05-01 16:20:18 -0400

    Created routing and views for this website

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//TUrn on error reporting
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function () {
+    $view = new Template();
+    echo $view->render('views/my-pets.html');
+});
+
+//Run fat-free
+$f3->run();
+?>
